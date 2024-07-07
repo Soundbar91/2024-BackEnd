28d85d076e669d17a0c001d9c24cdd494738cdda
신관규 <soundbar91@gmail.com>
2024-07-07 14:21:18 +0900

    feat: 쿠키 검증 로직 추가

---
+++ src/main/java/com/example/demo/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,28 @@
+package com.example.demo.interceptor;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import static jakarta.servlet.http.HttpServletResponse.*;
+
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("memberId")) {
+                    return true;
+                }
+            }
+        }
+
+        response.setStatus(SC_UNAUTHORIZED);
+        response.sendRedirect(request.getContextPath() + "/login");
+        return false;
+    }
+}
