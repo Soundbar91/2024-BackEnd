ec6e866db196ede57fc186679a35176e80a0af96
신관규 <soundbar91@gmail.com>
2024-07-09 15:35:25 +0900

    refactor: 로그인 로직 수정

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,14 +1,20 @@
 package com.example.demo.controller;
 
 import com.example.demo.controller.dto.request.LoginRequest;
+import com.example.demo.controller.dto.response.MemberResponse;
 import com.example.demo.domain.Member;
+import com.example.demo.exception.ApplicationException;
 import com.example.demo.service.LoginService;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
 
 @Controller
 public class LoginController {
@@ -20,18 +26,23 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(
+    public ResponseEntity<MemberResponse> login(
             @Valid @ModelAttribute LoginRequest loginRequest,
-            HttpServletResponse response
-    ) {
-        Member member = loginService.login(loginRequest);
-        if (member == null) return "redirect:/login";
+            HttpServletResponse response,
+            HttpServletRequest request
+    ) throws IOException {
+        MemberResponse member = loginService.login(loginRequest);
+        if (member == null) {
+            response.sendRedirect(request.getContextPath());
+            return ResponseEntity.badRequest().build();
+        }
 
-        Cookie cookie = new Cookie("memberId", String.valueOf(member.getId()));
+        Cookie cookie = new Cookie("memberId", String.valueOf(member.id()));
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
 
         response.addCookie(cookie);
-        return "redirect:/main";
+        response.sendRedirect(request.getContextPath() + "/main");
+        return ResponseEntity.ok(member);
     }
 }
--- src/main/java/com/example/demo/service/LoginService.java
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -1,10 +1,14 @@
 package com.example.demo.service;
 
 import com.example.demo.controller.dto.request.LoginRequest;
+import com.example.demo.controller.dto.response.MemberResponse;
 import com.example.demo.domain.Member;
+import com.example.demo.exception.ApplicationException;
 import com.example.demo.repository.MemberRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import static com.example.demo.exception.ErrorCode.MEMBER_NOT_FOUND;
 
 @Service
 public class LoginService {
@@ -17,11 +21,11 @@ public class LoginService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public Member login(LoginRequest loginRequest) {
-        Member member = memberRepository.findByEmail(loginRequest.email());
-        if (member == null) return null;
+    public MemberResponse login(LoginRequest loginRequest) {
+        Member member = memberRepository.findByEmail(loginRequest.email())
+                .orElseThrow(() -> new ApplicationException(MEMBER_NOT_FOUND));
 
         if (!passwordEncoder.matches(loginRequest.password(), member.getPassword())) return null;
-        else return member;
+        else return MemberResponse.from(member);
     }
 }
